f2be3c26749ff4b3ab1e71a18f8247b5708a76b8
anatoly <kaim@mail.ru>
2018-03-20 10:15:31 +0100

    тест

--- src/main/java/com/example/test1/Test1Application.java
+++ src/main/java/com/example/test1/Test1Application.java
@@ -3,7 +3,16 @@ package com.example.test1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @SpringBootApplication
 public class Test1Application {
@@ -12,12 +21,6 @@ public class Test1Application {
 		SpringApplication.run(Test1Application.class, args);
 	}
 
-	@Autowired
-	public void config(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-				.inMemoryAuthentication().withUser("user")
-				.password("pass")
-				.roles("USER");
-	}
+
 
 }
